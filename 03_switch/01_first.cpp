c56e98336923690258642f943bb8c55457ebb599
ArzoRaza <ccoding76@gmail.com>
2024-03-27 21:33:42 +0530

    Switch optr some changes

--- 03_switch/01_first.cpp
+++ 03_switch/01_first.cpp
@@ -4,17 +4,25 @@ using namespace std;
 int main() {
     int num = 0;
     switch (num) {
-        case 1 : cout<< "First number" << endl;
+        case 1 : cout<< "One number" << endl;
             break;
-        case 2 : cout<< "Second number" << endl;
+        case 2 : cout<< "Two number" << endl;
             break;
-        case 3 : cout<< "Third number" << endl;
+        case 3 : cout<< "Three number" << endl;
             break;
-        case 4 : cout<< "Third number" << endl;
+        case 4 : cout<< "Four number" << endl;
             break;
-        case 5 : cout<< "Third number" << endl; 
+        case 5 : cout<< "Five number" << endl; 
             break;
-        case 6 : cout<< "Third number" << endl; 
+        case 6 : cout<< "Six number " << endl; 
+            break;
+        case 7 : cout<< "Seven number" << endl;
+            break;
+        case 8 : cout<< "Eight number" << endl;
+            break; 
+        case 9 : cout<< "Nine number" << endl;
+            break;
+        case 10: cout<< "Ten number" << endl; 
             break;
         default : cout<< "focus on your input" << endl;
     }
