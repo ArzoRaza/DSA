93f298d7701ca9b84023a36220fcd091f4524658
ArzoRaza <ccoding76@gmail.com>
2024-03-27 21:26:21 +0530

    switch operators

---
+++ 03_switch/01_first.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int num = 0;
+    switch (num) {
+        case 1 : cout<< "First number" << endl;
+            break;
+        case 2 : cout<< "Second number" << endl;
+            break;
+        case 3 : cout<< "Third number" << endl;
+            break;
+        case 4 : cout<< "Third number" << endl;
+            break;
+        case 5 : cout<< "Third number" << endl; 
+            break;
+        case 6 : cout<< "Third number" << endl; 
+            break;
+        default : cout<< "focus on your input" << endl;
+    }
+}
