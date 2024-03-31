3608b5e0864bd6ed7598969b20030a049577bd7c
ArzoRaza <ccoding76@gmail.com>
2024-03-31 11:28:24 +0530

    Learning Array

---
+++ 05_array/01_demo.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    // declare array
+    int arr[14] = {1, 2, 3, 4, 5};
+
+    // accessing array
+    cout<< "chalo array sikhte hai :" << arr[4]  << endl;
+
+    // int n = 4;
+    // for ( int i = 0; i<=n; i++) {
+    //     cout<< arr[i] <<endl;
+    // }
+    int results[5] = {5,6,7};
+    int nullVal = 5; 
+    for (int i = 0; i<=nullVal; i++){
+        cout<< results[i] <<endl;
+    }
+
+
+    // we Explore lot of things tomorrow. 
+
+}
