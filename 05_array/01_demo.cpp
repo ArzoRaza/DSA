c19f84f95708a851c8ab6cbede592ab864757812
ArzoRaza <ccoding76@gmail.com>
2024-04-01 22:38:34 +0530

    Array Modified

--- 05_array/01_demo.cpp
+++ 05_array/01_demo.cpp
@@ -1,25 +1,51 @@
 #include <iostream>
 using namespace std;
 
+
+void printArray(int arr[], int size) {
+    cout<<"Print the array" << endl;
+
+    for(int i = 0; i<size; i++){
+        cout<<arr[i] << ""; 
+    }
+    cout<<"Printing Done" << endl;
+}
+
+
 int main(){
 
     // declare array
-    int arr[14] = {1, 2, 3, 4, 5};
+    // int arr[14] = {1, 2, 3, 4, 5};
+    int number[15];
 
     // accessing array
-    cout<< "chalo array sikhte hai :" << arr[4]  << endl;
+    cout<< "Chalo check kare :" << number[14]  << endl;
 
     // int n = 4;
     // for ( int i = 0; i<=n; i++) {
     //     cout<< arr[i] <<endl;
     // }
-    int results[5] = {5,6,7};
+    int results[15] = {5,6,7};
     int nullVal = 5; 
     for (int i = 0; i<=nullVal; i++){
         cout<< results[i] <<endl;
     }
+    
+    int fourth[10] = {0};
 
+    int n = 10;
+    cout<< "Printing the array" <<endl;
+    for(int i = 0; i<n; i++) {
+        cout<<fourth[i]<< "";
+    }
 
-    // we Explore lot of things tomorrow. 
+    int fifth[10]={1};
+    n= 10;
+    cout<<"Printing the array" <<endl;
+    for(int i = 0; i<n; i++) {
+        cout << fifth[i] << "";
+    }
+    cout << endl << "Everything is Fine" << endl << endl;
 
+    return 0;
 }
