1fc0e54deff07babebc9f5be9fa75a91445068c6
ArzoRaza <ccoding76@gmail.com>
2024-04-03 00:06:16 +0530

    And visitade

--- 05_array/02_typeOfArray.cpp
+++ 05_array/02_typeOfArray.cpp
@@ -2,6 +2,11 @@
 using namespace std;
 
 int main() {
-    char cha[10] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j'};
+
+    int num[10] = {1, 2, 3, 4, 5, 6, 7};
+    cout<< num[4] <<endl;
+
+
+    char cha[10] = {'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j'};
     cout<<(cha) <<endl;
 }
