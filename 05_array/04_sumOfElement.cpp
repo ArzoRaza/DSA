6e4b7d72af2dd21669cd3b27f1cb5b683eb9579e
ArzoRaza <ccoding76@gmail.com>
2024-04-04 10:17:54 +0530

    Sum of Element

---
+++ 05_array/04_sumOfElement.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+int arr[5] = {7, 2, 1, -4, 11};
+int sum =0;
+
+for (int i = 0; i < 5; i++ ){
+    sum += arr[i];
+}
+cout<<"Sum of Total number :" << sum <<endl;
+
+}
