5351c3ccf872e7fd0156ef12e57abe6a11ff1070
ArzoRaza <ccoding76@gmail.com>
2024-06-01 07:47:55 +0530

    some changes

---
+++ bitwise_operator/first.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+    int fun1(int n)
+    {
+        if(n&1)
+            return 0;
+        else
+            return 1;
+    }
+
---
+++ pattern/first.cpp
@@ -0,0 +1,20 @@
+// command to run this file: g++ -o run.exe filename
+// ./run.exe
+#include<iostream>
+using namespace std;
+
+int main(){
+    int n = 5;
+    // cin>>n;
+
+    int i = 1;
+    while(i<=n){
+        int j=i;
+        while(j<=n){
+            cout << "*";
+            j=j+1;
+        }
+        cout<<endl;
+        i=i+1;
+    }
+}
