1c12e3bf2aa4a1a785cb8f4a10beae31bc79c6e2
ArzoRaza <ccoding76@gmail.com>
2024-06-25 07:12:55 +0530

    some changes

--- pattern/2nd.cpp
+++ pattern/2nd.cpp
@@ -26,4 +26,21 @@ int main(){
     // 4 4 4 4
     // 5 5 5 5 5
 
+    // int n;
+    // cin>> n;
+
+    // int row = 1;
+
+    // while(row<=n){
+
+    //     int col = 1;
+
+    //     while(col<=row){
+    //         cout<<row + col - 1 << " ";
+    //         col = col + 1;
+    //     }
+    //     cout<< endl;
+    //     row = row + 1;
+    // }
+
 }
