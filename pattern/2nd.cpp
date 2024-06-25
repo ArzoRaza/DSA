c17c7105a1591c356e179707df5b645252051edc
ArzoRaza <ccoding76@gmail.com>
2024-06-25 07:00:27 +0530

    row pattern

--- pattern/2nd.cpp
+++ pattern/2nd.cpp
@@ -2,18 +2,28 @@
 using namespace std;
 
 int main(){
-    int n;
-    cin>>n;
 
-    int i = 1; 
-    while(i<=n){
-        int j = 1;
-        while(j<=i){
-            cout<<j;
-            j = j + 1;
-        }
-        cout<< " " << endl;
-        i = i + 1;
-    }
+    // row tringle  
+    // int n;
+    // cin>>n;
+
+    // int i = 1; 
+    // while(i<=n){
+
+    //     int j = 1;
+
+    //     while(j<=i){
+    //         cout<<i<< " "; 
+    //         j = j + 1;
+    //     }
+    //     cout<< " " << endl;
+    //     i = i + 1;
+    // }
+    // Output: 
+    // 1
+    // 2 2
+    // 3 3 3
+    // 4 4 4 4
+    // 5 5 5 5 5
 
 }
