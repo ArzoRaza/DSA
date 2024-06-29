e7280cee83af1bdc8838707565e62a64e2658982
ArzoRaza <ccoding76@gmail.com>
2024-06-29 23:16:16 +0530

    some changes

--- pattern/2nd.cpp
+++ pattern/2nd.cpp
@@ -4,21 +4,21 @@ using namespace std;
 int main(){
 
     // row tringle  
-    // int n;
-    // cin>>n;
+    int n;
+    cin>>n;
 
-    // int i = 1; 
-    // while(i<=n){
+    int i = 1; 
+    while(i<=n){
 
-    //     int j = 1;
+        int j = 1;
 
-    //     while(j<=i){
-    //         cout<<i<< " "; 
-    //         j = j + 1;
-    //     }
-    //     cout<< " " << endl;
-    //     i = i + 1;
-    // }
+        while(j<=i){
+            cout<<i<< " "; 
+            j = j + 1;
+        }
+        cout<< " " << endl;
+        i = i + 1;
+    }
     // Output: 
     // 1
     // 2 2
@@ -26,57 +26,59 @@ int main(){
     // 4 4 4 4
     // 5 5 5 5 5
 
-    // int n;
-    // cin>> n;
+    int n;
+    cin>> n;
 
-    // int row = 1;
+    int row = 1;
 
-    // while(row<=n){
+    while(row<=n){
 
-    //     int col = 1;
+        int col = 1;
 
-    //     while(col<=row){
-    //         cout<<row + col - 1 << " ";
-    //         col = col + 1;
-    //     }
-    //     cout<< endl;
-    //     row = row + 1;
-    // }
+        while(col<=row){
+            cout<<row + col - 1 << " ";
+            col = col + 1;
+        }
+        cout<< endl;
+        row = row + 1;
+    }
 
 
-    // int n;
-    // cin>> n;
+    int n;
+    cin>> n;
 
-    // int i = 1;
+    int i = 1;
 
-    // while (i<=n){
+    while (i<=n){
 
-    //     int j = 1;
-    //     while(j<=i){
-    //         cout<< i - j + 1;
-    //         j = j + 1;
-    //     }
-    //     cout<< endl;
-    //     i = i + 1;
-    // }
+        int j = 1;
+        while(j<=i){
+            cout<< i - j + 1;
+            j = j + 1;
+        }
+        cout<< endl;
+        i = i + 1;
+    }
 
 
 // character pattern 
-    // int n;
-    // cin>>n;
+    int n;
+    cin>>n;
 
-    // int i = 1;
-    // while(i<=n){
-    //     int j = 1;
-    //     while(j<=n){
-    //         char ch = 'A' + i - 1;
-    //         cout << ch << " ";
-    //         j = j + 1;
-    //     }
-    //     cout<< endl;
-    //     i = i + 1;
-    // }
+    int i = 1;
+    while(i<=n){
+        int j = 1;
+        while(j<=n){
+            char ch = 'A' + i - 1;
+            cout << ch << " ";
+            j = j + 1;
+        }
+        cout<< endl;
+        i = i + 1;
+    }
+
 
 
 
 }
+    
