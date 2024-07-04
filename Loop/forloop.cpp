052005fda5c155cb602b27591080ca2871c8e931
ArzoRaza <ccoding76@gmail.com>
2024-07-04 23:05:21 +0530

    initialized some changes

---
+++ Loop/forloop.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    cout<< "Hello arzo" << endl;
+    int num = 5; 
+
+    for(int i = 1; i <= num; i++){
+        cout<< i << endl;
+    }
+}
