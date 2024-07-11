e8ed56b9d579d041be39e5ef66ad8df0c56ddc0d
ArzoRaza <ccoding76@gmail.com>
2024-07-11 23:36:17 +0530

    some changes

--- 04_function/03_funs.cpp
+++ 04_function/03_funs.cpp
@@ -1,14 +1,14 @@
-#include <iostream>
-using namespace std; 
+// #include <iostream>
+// using namespace std; 
 
-bool isEven(int a){
-    if (a&1){
-        return 0;
-    }
-    else{
-        return 1;
-    }
-}
+// bool isEven(int a){
+//     if (a&1){
+//         return 0;
+//     }
+//     else{
+//         return 1;
+//     }
+// }
 
 
 // int main () {
@@ -23,14 +23,14 @@ bool isEven(int a){
 //     }
 // }
 
-int main () {
-    int number;
-    cin>>number;
+// int main () {
+//     int number;
+//     cin>>number;
 
-    if (isEven(number)){
-        cout<<"number is Even " <<endl;
-    }
-    else{
-        cout<<"number is odd" <<endl;
-    }
-}
+//     if (isEven(number)){
+//         cout<<"number is Even " <<endl;
+//     }
+//     else{
+//         cout<<"number is odd" <<endl;
+//     }
+// }
