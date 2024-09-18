79e724a27e11b0091743137ba4571016472a0be2
ArzoRaza <ccoding76@gmail.com>
2024-09-18 13:57:00 +0530

    New Bank program Initialized

--- java_folder/01_program/NewBank.java
+++ java_folder/01_program/NewBank.java
@@ -1,8 +1,25 @@
 class Account{
+    String name;
     int Account;
     int balance;
-    String name;
 
+    public Account(String name, int Account, int balance){
+        this.name = name;
+        this.Account = Account;
+        this.balance = balance;
+    }
 
+    public void DesplayDetails(){
+        System.out.println("Account Name: " + name);
+        System.out.println("Account Number: " + Account);
+        System.out.println("Balance: " + balance);
+    }
+}
 
+public class NewBank{
+    public static void main(String[] args) {
+        Account account1 = new Account("Md Akbar Ali", 123456789, 2000);
+
+        account1.DesplayDetails();
+    }
 }
