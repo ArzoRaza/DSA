ca63558f8690740fdb1494f306fc3c065ab19ff5
ArzoRaza <ccoding76@gmail.com>
2024-09-20 09:00:44 +0530

    Update code initialization

--- java_folder/01_program/NewBank.java
+++ java_folder/01_program/NewBank.java
@@ -1,25 +1,25 @@
 class Account{
     String name;
-    int Account;
-    int balance;
+    long accountNo;
+    long balance;
 
-    public Account(String name, int Account, int balance){
+    public Account(String name, long accountNo, long balance){
         this.name = name;
-        this.Account = Account;
+        this.accountNo = accountNo;
         this.balance = balance;
     }
 
-    public void DesplayDetails(){
+    public void ResultShowTime(){
         System.out.println("Account Name: " + name);
-        System.out.println("Account Number: " + Account);
-        System.out.println("Balance: " + balance);
+        System.out.println("Account Number: " + accountNo);
+        System.out.println("Account Balance: " + balance);
     }
 }
 
 public class NewBank{
     public static void main(String[] args) {
-        Account account1 = new Account("Md Akbar Ali", 123456789, 2000);
+        Account account1 = new Account("Md Akbar Ali", 1022022090877L, 5000);
 
-        account1.DesplayDetails();
+        account1.ResultShowTime();
     }
 }
