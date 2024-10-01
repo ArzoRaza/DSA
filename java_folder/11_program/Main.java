695411d3aee19d088e849ec3e7d5b6006ba3e0a2
ArzoRaza <ccoding76@gmail.com>
2024-10-01 10:17:02 +0530

    updated file updated

--- java_folder/11_program/Main.java
+++ java_folder/11_program/Main.java
@@ -3,7 +3,7 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        List<String> names = Arrays.asList("Alice", "Bob", "Charlie", "David", "Edward");
+        List<String> names = Arrays.asList("Alice", "Bob", "Charlie", "David", "Akbar", "Arzo", "Edward");
 
         // Filter names starting with 'A' and print them
         names.stream()
