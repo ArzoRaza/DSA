4291528557d0b169a76f63a43021d75150f9dde1
ArzoRaza <ccoding76@gmail.com>
2024-10-09 20:10:24 +0530

    new file adding

---
+++ Array_folder/01_program/Main.java
@@ -0,0 +1,8 @@
+import java.util.Arrays;
+
+public class Main{
+    public static void main(String[] args) {
+        int[] arr = {2, 3, 4, 3};
+        System.out.println(Arrays.toString(arr));
+    }
+}
