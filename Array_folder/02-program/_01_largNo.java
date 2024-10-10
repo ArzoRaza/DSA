1d0323667e67a9874f1e4342d696257a21c25493
ArzoRaza <ccoding76@gmail.com>
2024-10-10 11:01:19 +0530

    Find the largest Value

---
+++ Array_folder/02-program/_01_largNo.java
@@ -0,0 +1,15 @@
+public class _01_largNo{
+    public static void main(String[] args) {
+        int[] numbers = {10, 5, 3, 12, 8};
+        int largNum = numbers[0];
+        int count = 0;
+        for (int i = 0; i < numbers.length; i++) {
+            if(numbers[i] > largNum){
+                largNum = numbers[i];
+            }
+        }
+        System.out.println(largNum);
+        System.out.println(count);
+    }
+    
+}
